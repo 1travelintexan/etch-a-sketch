b4125789b5eab04315cd95bdcfd500fa53c60d61
Joshua George <horheyinc8@gmail.com>
2022-01-04 17:07:35 +0100

    screen drawing working

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-let container = document.querySelector("#container");
-
+let screen = document.querySelector("#screen");
 for (let i = 0; i < 16; i++) {
   for (let i = 0; i < 16; i++) {
     let box = document.createElement("div");
     box.className = "box";
-    container.appendChild(box);
+    screen.appendChild(box);
   }
 }
+let boxes = document.querySelectorAll(".box");
+
+console.log(boxes);
+boxes.forEach((box) => {
+  box.addEventListener("mouseover", () => {
+    box.classList.add("box-selected");
+  });
+});
